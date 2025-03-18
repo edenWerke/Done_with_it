154e1defcc7d9bb53702f48f45184de6736a60cf
edenWerke <edenw6292@gmail.com>
2025-03-18 10:05:03 +0300

    flexbox

--- App.js
+++ App.js
@@ -17,50 +17,13 @@ import {
 } from "@react-native-community/hooks";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-  const handlePress = () => {
-    console.log("text pressed");
-  };
-
   return (
-    // the right side over
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      {/* <Button
-        color="orangered"
-        title="Click Me"
-        onPress={() =>
-          Alert.alert("My Title", "Are you sure?", [
-            {
-              text: "Yes",
-              onPress: () => alert("You clicked yes"),
-            },
-            {
-              text: "No",
-              onPress: () => alert("You clicked no"),
-            },
-          ])
-        }
-      /> */}
-      <View
-        style={{
-          backgroundColor: "darkblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
-        }}
-      ></View>
-    </SafeAreaView>
+    <View style={{ backgroundColor: "pink", flex: 1 }}>
+      <View style={{ backgroundColor: "gold", flex: 2 }} />
+      <View style={{ backgroundColor: "dodgerblue", flex: 1 }} />
+      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+    </View>
   );
 }
 
-const containerStyle = { backgroundColor: "yellow" };
-//using the satus par in order to know the perfect padding
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "dodgerblue",
-    // alignItems: "center",
-    // justifyContent: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
 //LAYOUTS
