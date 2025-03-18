23a9eddaef4249b095e7e89d71ace6ad1104c36f
edenWerke <edenw6292@gmail.com>
2025-03-18 13:15:01 +0300

    adding view for login button

--- app/screens/WelcomeScreen.jsx
+++ app/screens/WelcomeScreen.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { ImageBackground, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, View, SafeView } from "react-native";
 function WelcomeScreen(props) {
   return (
     <ImageBackground
       style={styles.background}
       source={require("../assets/Background.jpg")}
-    ></ImageBackground>
+    >
+      <View style={styles.loginButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: "flex-end",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "tomato",
   },
 });
 export default WelcomeScreen;
