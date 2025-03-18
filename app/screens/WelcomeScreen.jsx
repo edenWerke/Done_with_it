77a62abc36181a9c4f75473f979ae3c5ac4a5ed5
edenWerke <edenw6292@gmail.com>
2025-03-18 13:31:45 +0300

    adding a logo

--- app/screens/WelcomeScreen.jsx
+++ app/screens/WelcomeScreen.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View, SafeView } from "react-native";
+import { ImageBackground, StyleSheet, View, Image } from "react-native";
 function WelcomeScreen(props) {
   return (
     <ImageBackground
       style={styles.background}
       source={require("../assets/Background.jpg")}
     >
+      <Image style={styles.logo} source={require("../assets/logo.jpg")} />
       <View style={styles.loginButton}></View>
       <View style={styles.RegisterButton}></View>
     </ImageBackground>
@@ -16,6 +17,7 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
   },
   loginButton: {
     width: "100%",
@@ -26,6 +28,12 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 70,
     backgroundColor: "dodgerblue",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 70,
   },
 });
 export default WelcomeScreen;
