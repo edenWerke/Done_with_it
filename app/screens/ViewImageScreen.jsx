6550b1f96deafe6f9157b93ca14c169f4f810eee
edenWerke <edenw6292@gmail.com>
2025-03-18 14:07:54 +0300

    adding viewImage screen and close icon place holder

---
+++ app/screens/ViewImageScreen.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+function ViewImageScreen(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.CloseIcon}></View>
+      <Image style={styles.image} source={require("../assets/cloth.jpg")} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain", // Ensures the entire image is visible
+  },
+  container: {
+    backgroundColor: "pink",
+    flex: 1,
+  },
+  CloseIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "tomato",
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+});
+
+export default ViewImageScreen;
